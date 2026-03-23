01f42e925b50db9dfa9aaed66cd4e2a6904cc175
filipetaremba <filipetaremba0@gmail.com>
2026-03-23 21:21:37 +0200

    Update Routes
    Add new routes to site

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/sobre', 'Home::sobre');
+$routes->get('/servicos', 'Home::servicos');
+$routes->get('/projetos', 'Home::projetos');
+$routes->get('/contacto', 'Home::contacto');
