1563845390709970acc60030e57fd24152f63f55
filipetaremba <filipetaremba0@gmail.com>
2026-03-23 21:28:11 +0200

    Update Home
    Adicinei conteudo no Controller do home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,59 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        $data['lang'] = session()->get('lang') ?? 'pt';
+        return view('layouts/main', [
+            'page'    => 'pages/home',
+            'lang'    => $data['lang'],
+            'title'   => 'ConstrucaoMz'
+        ]);
+    }
+
+    public function sobre()
+    {
+        $lang = session()->get('lang') ?? 'pt';
+        return view('layouts/main', [
+            'page'  => 'pages/sobre',
+            'lang'  => $lang,
+            'title' => 'Sobre Nós | ConstrucaoMz'
+        ]);
+    }
+
+    public function servicos()
+    {
+        $lang = session()->get('lang') ?? 'pt';
+        return view('layouts/main', [
+            'page'  => 'pages/servicos',
+            'lang'  => $lang,
+            'title' => 'Serviços | ConstrucaoMz'
+        ]);
+    }
+
+    public function projetos()
+    {
+        $lang = session()->get('lang') ?? 'pt';
+        return view('layouts/main', [
+            'page'  => 'pages/projetos',
+            'lang'  => $lang,
+            'title' => 'Projetos | ConstrucaoMz'
+        ]);
+    }
+
+    public function contacto()
+    {
+        $lang = session()->get('lang') ?? 'pt';
+        return view('layouts/main', [
+            'page'  => 'pages/contacto',
+            'lang'  => $lang,
+            'title' => 'Contacto | ConstrucaoMz'
+        ]);
+    }
+
+    public function setLang($lang = 'pt')
+    {
+        session()->set('lang', $lang);
+        return redirect()->back();
     }
 }
